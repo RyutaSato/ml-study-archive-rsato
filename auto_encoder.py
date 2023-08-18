c3aeea4758d1514495c91ddac6c5c85edcf1b1ab
rsato <sato.ryuta.main@gmail.com>
2023-08-18 23:37:29 +0900

    :sparkle: reflect recent changes

--- auto_encoder.py
+++ auto_encoder.py
@@ -10,9 +10,9 @@ def main():
     y = y.map(lambda x: attack_label_class[x]).map(lambda x: correspondences[x])
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=RANDOM_SEED, stratify=y)
     model = Sequential([
-        Dense(units=10, activation='relu', input_dim=38, name='encoder1'),
-        Dense(units=5, activation='relu', name='encoder2'),
-        Dense(units=10, activation='relu'),
+        Dense(units=20, activation='relu', input_dim=38, name='encoder1'),
+        Dense(units=10, activation='relu', name='encoder2'),
+        Dense(units=20, activation='relu'),
         Dense(units=38, activation='relu'),
     ])
     model.compile(optimizer='adam', loss='mean_squared_error')
@@ -21,20 +21,20 @@ def main():
         model.get_layer('encoder1'),
         model.get_layer('encoder2')
     ])
-    columns = list(map(lambda x: 'feature' + str(x), range(5)))
+    columns = list(map(lambda x: 'feature' + str(x), range(10)))
     x_train_encoded = pd.DataFrame(data=encoders.predict(x_train), index=x_train.index, columns=columns)
     x_test_encoded = pd.DataFrame(data=encoders.predict(x_test), index=x_test.index, columns=columns)
     x_train_new_feature = x_train.merge(x_train_encoded, right_index=True, left_index=True)
     x_test_new_feature = x_test.merge(x_test_encoded, right_index=True, left_index=True)
-    x_train_encoded.to_pickle("models/kdd99_features/x_train_encoded_5_df&activation=relu&epochs=5&batch_size=32.pkl")
-    x_test_encoded.to_pickle("models/kdd99_features/x_test_encoded_5_df&activation=relu&epochs=5&batch_size=32.pkl")
-    x_train_new_feature.to_pickle("models/kdd99_features/x_train+ae_43_df&activation=relu&epochs=5&batch_size=32"
+    x_train_encoded.to_pickle("models/kdd99_features/x_train_encoded_10_df&activation=relu&epochs=5&batch_size=32.pkl")
+    x_test_encoded.to_pickle("models/kdd99_features/x_test_encoded_10_df&activation=relu&epochs=5&batch_size=32.pkl")
+    x_train_new_feature.to_pickle("models/kdd99_features/x_train+ae_48_df&activation=relu&epochs=5&batch_size=32"
                                   ".pkl")
-    x_test_new_feature.to_pickle("models/kdd99_features/x_test+ae_43_df&activation=relu&epochs=5&batch_size=32.pkl")
-    y_train.to_pickle("models/kdd99_features/y_train_df.pkl")
-    y_test.to_pickle("models/kdd99_features/y_test_df.pkl")
-    x_train.to_pickle("models/kdd99_features/x_train_df.pkl")
-    x_test.to_pickle("models/kdd99_features/x_test_df.pkl")
+    x_test_new_feature.to_pickle("models/kdd99_features/x_test+ae_48_df&activation=relu&epochs=5&batch_size=32.pkl")
+    # y_train.to_pickle("models/kdd99_features/y_train_df.pkl")
+    # y_test.to_pickle("models/kdd99_features/y_test_df.pkl")
+    # x_train.to_pickle("models/kdd99_features/x_train_df.pkl")
+    # x_test.to_pickle("models/kdd99_features/x_test_df.pkl")
 
 
 if __name__ == '__main__':
