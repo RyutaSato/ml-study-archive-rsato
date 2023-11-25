8e813dccd74e372aa073a40fa7b32a60e851046c
rsato <sato.ryuta.main@gmail.com>
2023-11-26 02:53:28 +0900

    :zap: 親プロセスに例外処理を追加

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 このプログラムは、原則変更禁止。
 非破壊的変更を加える際は、パッチバージョンを上げること。
 
-Supported BaseFlow: 1.2.0
+Supported BaseFlow: 1.2.4
 """
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, as_completed, Future
 from itertools import product
 from multiprocessing import cpu_count
 
@@ -12,12 +12,13 @@ from loguru import logger
 
 from _main import flows, runners, load_config
 
+
 def main():
     cfg = load_config()
-    with ProcessPoolExecutor() as executor:
-        futures = dict()
+    with ProcessPoolExecutor(max_workers=2) as executor:
+        futures: dict[str, Future] = dict()
         for layers, model, dataset \
-            in product(cfg['layers'], cfg['models'], cfg['datasets']):
+                in product(cfg['layers'], cfg['models'], cfg['datasets']):
             params = cfg['default'].copy()
             params['encoder_param']['layers'] = layers
             Flow = flows[dataset['name']]
@@ -26,13 +27,17 @@ def main():
                     pass
                 else:
                     params[k] = v
-                    
+
             runner = runners[model]
 
             future = runner(params, executor, Flow)
             futures[f"{model}{layers}{dataset['name']}"] = future
         for k in futures:
-            logger.info(f"{k} is done: {futures[k].result()}")
+            #
+            try:
+                logger.info(f"{k} is done: {futures[k].result()}")
+            except Exception as e:
+                logger.error(f"task {k} raised error: {e}")
 
 
 if __name__ == '__main__':
