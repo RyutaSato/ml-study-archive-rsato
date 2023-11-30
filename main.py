2b99a9a0c84eb36e8ecbfba0492a9a3cf999dc07
rsato <sato.ryuta.main@gmail.com>
2023-11-30 23:21:00 +0900

    :zap: v1.4.0 モデルの読み込みと学習が重複しないように順番を変更

--- main.py
+++ main.py
@@ -13,21 +13,22 @@ from _main import flows, runners, load_config
 def main():
     cfg = load_config()
     with ProcessPoolExecutor(max_workers=2) as executor:
-        for dataset, model, layers \
-                in product(cfg['datasets'], cfg['models'], cfg['layers']):
-            params = cfg['default'].copy()
-            params['encoder_param']['layers'] = layers
-            Flow = flows[dataset['name']]
-            for k, v in dataset.items():
-                if k == 'name':
-                    pass
-                else:
-                    params[k] = v
-
-            runner = runners[model]
-
-            future = runner(params, executor, Flow)
-            future.add_done_callback(clean_up)
+        for model, dataset, layers \
+                in product(cfg['models'], cfg['datasets'], cfg['layers']):
+            print(dataset, model, layers)
+            # params = cfg['default'].copy()
+            # params['encoder_param']['layers'] = layers
+            # Flow = flows[dataset['name']]
+            # for k, v in dataset.items():
+            #     if k == 'name':
+            #         pass
+            #     else:
+            #         params[k] = v
+            #
+            # runner = runners[model]
+            #
+            # future = runner(params, executor, Flow)
+            # future.add_done_callback(clean_up)
 
 
 def clean_up(return_value):
