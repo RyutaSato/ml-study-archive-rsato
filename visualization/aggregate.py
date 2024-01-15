b762aa65e2c902007ec6bd7ae658a1b0d09f1638
Ryuta Sato <sato.ryuta.main@gmail.com>
2024-01-15 22:26:55 +0900

    :recycle: refactored filter

--- visualization/aggregate.py
+++ visualization/aggregate.py
@@ -6,17 +6,21 @@ import pandas as pd
 def filter_results():
     with open("logs/results.v.2.0.0.json") as f:
         results: dict[str, list[dict]] = json.load(f)
-    filtered_list = []
+    filtered_list = dict()
     for dataset_name in results:
         dataset_results = results[dataset_name]
         for result in dataset_results:
             if result['model']['optuna']:
                 continue
             if not result['dataset']['standardization']:
-                pass
+                continue
+            if not result['ae']['standardization']:
+                continue
+            if not result['ae']['used_class'] == 'all':
+                continue
 
 
-            filtered_list.append({
+            filtered_list[result['hash']] = ({
                 'dataset': result['dataset']['name'],
                 'model': result['model']['name'],
                 'layers': str(result['ae']['layers']),
@@ -24,7 +28,7 @@ def filter_results():
                 'minority': result['result']['minority']['f1'],
                 'macro': result['result']['macro']['f1'],
             })
-    df = pd.DataFrame(filtered_list)
+    df = pd.DataFrame(filtered_list.values())
     df.to_csv("logs/filtered_results.csv", index=False)
 
 if __name__ == '__main__':
