523a98e112649bcb2a7425404b447d5c90ec12b8
Ryuta Sato <sato.ryuta.main@gmail.com>
2024-01-22 12:58:49 +0900

    :rocket: プロセスのkillをHTTPリクエストできる機能

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ app = FastAPI()
 queue = Queue()
 lock = Lock()
 processes = []
+not_finished = []
 
 
 @app.on_event("startup")
@@ -33,10 +34,17 @@ def run(params: Params):
     queue.put(params)
     return {"message": "ok"}
 
+@app.get("kill", status_code=200)
+def kill():
+    while not queue.empty():
+        not_finished.append(queue.get().json())
+    for _ in processes:
+        queue.put(None)
+    return {"message": "ok"}
+
 
 @app.on_event("shutdown")
 def shutdown_event():
-    not_finished = []
     while not queue.empty():
         not_finished.append(queue.get().json())
     # Read existing data from the file
